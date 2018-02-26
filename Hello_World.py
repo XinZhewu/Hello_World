d6e027f67b23914c8f6f15fd2349f44f1c5a635d
锌锗钨 <568580410@qq.com>
2018-02-26 10:37:18 -0600

    Add files via upload

---
+++ Hello_World.py
@@ -0,0 +1,26 @@
+# coding=utf-8
+
+hi = '\nHello World!~'
+print(hi)
+
+answer = input(
+    '\nCould I know what to call you?'
+    "\n(Just a real name, please. (￣▽￣)'' I'm a novice, you know that.)"
+)
+
+message = (
+    '\nOK~ Nice to meet you! ' + answer.title() + '.'
+    '\nI am XinZhewu, a Chinese college student. (。・∀・)ノ'
+    '\n\nI started learning Python in December 4, 2017.'
+    '\nIt may be have a little regrettable.'
+    '\nBecause I was late to know the GitHub community and started to use it.'
+    '\n\nThis is the first program I wrote. And I know it looks very simple.'
+    '\nBut if you see this program, I hope you have a pleasant day.'
+    '\n\nHere is my way of contact,'
+    '\nIf you have anything to teach me or want to make a new friend,'
+    ' contact me directly.'
+    '\nMy Email: 568580410@qq.com'
+    '\nMy Weibo: https://weibo.com/zwg568580410/'
+)
+
+print(message)
